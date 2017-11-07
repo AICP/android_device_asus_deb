a8441fe3bdf80ad669dea443914b2fb24d4e5e1e
Zips <hcolmenares@gmail.com>
2017-11-06 22:13:49 -0500

    deb: init oreo
    Change-Id: Ib15e08bfa72e00168614dad8b2fe0018fe280d7f

---
+++ aicp.mk
@@ -0,0 +1,35 @@
+# Release name
+PRODUCT_RELEASE_NAME := deb
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aicp/config/common_full_tablet_wifionly.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/config/telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/full_deb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := aicp_deb
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7 2013 LTE
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razorg \
+    BUILD_FINGERPRINT=google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys \
+    PRIVATE_BUILD_DESC="razorg-user 6.0.1 MOB30X 3036618 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Andrew Vance (Startrek852), doc HD (semdoc)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
